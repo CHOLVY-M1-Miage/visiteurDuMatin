724def0be84a5341c4383a51a95a873531b4295c
CHOLVY-M1-Miage <ethan.ek23@gmail.com>
2022-12-05 15:52:07 +0100

    ajout de la librairie jsonParse

--- src/test/java/edu/uga/miage/m1/polygons/gui/persistence/JsonTest.java
+++ src/test/java/edu/uga/miage/m1/polygons/gui/persistence/JsonTest.java
@@ -1,9 +1,13 @@
 package edu.uga.miage.m1.polygons.gui.persistence;
 
 import edu.uga.miage.m1.polygons.gui.shapes.SimpleShape;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +16,13 @@ public class JsonTest {
     void test_lecture_file_groupLess() throws FileNotFoundException {
         List<SimpleShape> shapes = new ArrayList<>();
 
-        //Object ob = new JSONParser().parse(new FileReader("JSONFile.json"));
+        try {
+            Object ob = new JSONParser().parse(new FileReader("JSONFile.json"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
