16c0bbbf75fd85e0d4ba51bda3c6aa6152ce1774
CHOLVY-M1-Miage <ethan.ek23@gmail.com>
2022-12-09 00:02:39 +0100

    ajout des commandes undo & forward [non testé]

--- src/main/java/edu/uga/miage/m1/polygons/gui/RemoteControl.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/RemoteControl.java
@@ -10,6 +10,7 @@ public class RemoteControl {
 
     private List<SimpleShape> listeShapes;
     protected List<Command> commands;
+    private int index = 0;
 
     public RemoteControl(List<SimpleShape> listeShapes) {
         this.listeShapes = listeShapes;
@@ -17,21 +18,32 @@ public class RemoteControl {
     }
 
     public boolean addCommand(Command command) {
+        //Supprime les commandes du forward
+        for (int i = index;i < this.commands.size();i++){
+            this.commands.remove(this.commands.size() - 1);
+        }
+        index++;
         return commands.add(command);
     }
 
     public void play(){
-        for (Command command : commands) {
-            command.execute();
+        for (int i = 0;i < index;i++){
+            this.commands.get(i).execute();
         }
     }
 
     public void undo(){
-        this.commands.remove(this.commands.size() - 1);
+        if (this.index > 1) {
+            this.index--;
+        }
         this.play();
     }
 
-    public void reset() {
-        commands.clear();
+    public void forward(){
+        if (this.index < this.commands.size()) {
+            this.index++;
+        }
+        this.play();
     }
+
 }
